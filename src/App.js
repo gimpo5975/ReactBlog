b490ce5fe6d6979eabc448ee79e03602229bbc39
Wonjung <gimpo5975@naver.com>
2024-04-23 16:31:52 +0900

    글 제목 클릭시 Modal 추가

--- src/App.js
+++ src/App.js
@@ -1,72 +1,80 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  let[글제목, setTitle] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
-  let[글내용, setContent] = useState('2월 17일 발행');
-  let[따봉, 따봉변경] =  useState(0);
-
+  let [글제목, setTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
+  let [글내용, setContent] = useState("2월 17일 발행");
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
-        <div className='black-nav'>
-          <h4>ReactBlog</h4>
-          <div className='buttonBox'>
-          <button onClick={()=>{
-            let copy = [...글제목];
-            copy[0] = '여자 코트 추천';
-            setTitle(copy);
-            }}>제목변환</button>
-            <button onClick={()=>{
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+        <div className="buttonBox">
+          <button
+            onClick={() => {
+              let copy = [...글제목];
+              copy[0] = "여자 코트 추천";
+              setTitle(copy);
+            }}
+          >
+            제목변환
+          </button>
+          <button
+            onClick={() => {
               let copy2 = [...글제목];
               copy2.sort();
               setTitle(copy2);
-            }}>제목정렬</button>
-            </div>
+            }}
+          >
+            제목정렬
+          </button>
         </div>
-        <div className='list'>
-          <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉} 
+      </div>
+      <div className="list">
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>
+          {따봉}
         </h4>
-          <p>{글내용}</p>
-        </div>
-        <div className='list'>
-          <h4>{글제목[1]}</h4>
-          <p>{글내용}</p>
-        </div>
-        <div className='list'>
-          <h4>{글제목[2]}</h4>
-          <p>{글내용}</p>
-        </div>
-        <Modal></Modal>
-      
+        <p>{글내용}</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>{글내용}</p>
+      </div>
+      <div className="list">
+        <h4 onClick={()=>{
+          {setModal(true)}
+        }}>{글제목[2]}</h4>
+        <p>{글내용}</p>
+      </div>
+      {
+        modal==true ? <Modal/> : null  
+      }
     </div>
   );
-}//end of App
+} //end of App
 
-
-
-function Modal(){
-  return(
-    <div className='modal'>
-    <h4>제목</h4>
-    <Title></Title>
-    <p>날짜</p>
-    <p>상세내용</p>
-    <Content/>
-  </div>
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   );
-}//end of modal
-
-function Title(){
-  return(
-    <p>나는 제목이다</p>
-  );
-}
-function Content(){
-  return(
-    <p>나는 내용이야</p>
-  );
-}
+} //end of modal
 
 export default App;
